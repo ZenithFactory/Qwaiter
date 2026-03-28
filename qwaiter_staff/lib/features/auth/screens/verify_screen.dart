8af07d5b7fc8355042a2c1bbe32e691c35e81c18
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-28 17:53:44 +0100

    feat(QW-05): add VerifyScreen with dispose function

---
+++ qwaiter_staff/lib/features/auth/screens/verify_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../auth_provider.dart';
+
+class VerifyScreen extends StatefulWidget {
+  const VerifyScreen({super.key});
+
+  @override
+  State<VerifyScreen> createState() => _VerifyScreenState();
+}
+
+class _VerifyScreenState extends State<VerifyScreen> {
+  final _codeController = TextEditingController();
+
+  @override
+  void dispose() {
+    _codeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
