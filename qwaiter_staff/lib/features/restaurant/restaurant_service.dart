86ce536bac66b32552931e774e922c21a4bf3b07
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-29 11:52:29 +0200

    feat(QW-05): add getRestaurants to RestaurantService

--- qwaiter_staff/lib/features/restaurant/restaurant_service.dart
+++ qwaiter_staff/lib/features/restaurant/restaurant_service.dart
@@ -3,4 +3,13 @@ import '../../core/network/dio_client.dart';
 
 class RestaurantService {
   final Dio _dio = DioClient().dio;
+
+  Future<List<dynamic>> getRestaurant() async {
+    try {
+      final response = await _dio.get('/user/restaurants');
+      return response.data as List<dynamic>;
+    } on DioException catch (e) {
+      throw e.response?.data['message'] ?? 'Failed to fetch restaurants';
+    }
+  }
 }
